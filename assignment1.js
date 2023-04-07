c949f1166280da8e1d690eaf7d3870a2411893aa
jeffreykng@hotmail.com <jeffreyng@Jeffreys-MBP.fios-router.home>
2023-04-07 08:21:29 -0400

    first assignment

---
+++ assignment1.js
@@ -0,0 +1,15 @@
+function countDown(num){
+    for (num = num; num>0; num --)
+    {console.log(num)}
+    console.log("DONE!")
+}
+
+function randomGame(){
+
+for (let i = 0; i >=0; i++) {let select = setInterval(Math.random(1), 1000)
+    }
+    if (select > .75) {
+        console.log(i)}
+    else {console.log(select)}
+    }
+
